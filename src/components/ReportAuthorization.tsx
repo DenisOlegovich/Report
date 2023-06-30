bb8433d3df8af6734a407995173d1ee0e2401101
DenisOlegovich <den9055902096@gmail.com>
2023-06-30 21:15:10 +0300

    Commit

--- src/components/ReportAuthorization.tsx
+++ src/components/ReportAuthorization.tsx
@@ -19,23 +19,11 @@ function ReportAuthorization() {
   </MenuButton>
   <MenuList>
     <MenuItem minH='48px'>
-      <Image
-        boxSize='2rem'
-        borderRadius='full'
-        src='https://placekitten.com/100/100'
-        alt='Fluffybuns the destroyer'
-        mr='12px'
-      />
+      
       <span>Fluffybuns the Destroyer</span>
     </MenuItem>
     <MenuItem minH='40px'>
-      <Image
-        boxSize='2rem'
-        borderRadius='full'
-        src='https://placekitten.com/120/120'
-        alt='Simon the pensive'
-        mr='12px'
-      />
+     
       <span>Simon the pensive</span>
     </MenuItem>
   </MenuList>
